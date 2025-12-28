6f4e789b7d6b3836a8b05524f47572fa445b932e
zeropse <129523087+zeropse@users.noreply.github.com>
2025-12-29 00:37:31 +0530

    fix minor errors

--- src/components/ui/drawer.jsx
+++ src/components/ui/drawer.jsx
@@ -38,6 +38,7 @@ function DrawerContent({ className, children, ...props }) {
       <DrawerOverlay />
       <DrawerPrimitive.Content
         data-slot="drawer-content"
+        aria-describedby={undefined}
         className={cn(
           "group/drawer-content bg-background fixed z-50 flex h-auto flex-col",
           "data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b",
